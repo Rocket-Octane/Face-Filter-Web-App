0010493474410be99a17db260b5a0e5763a2e53e
Rocket-Octane <93807708+Rocket-Octane@users.noreply.github.com>
2022-06-17 17:55:22 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,33 @@
-function preload(){
+function preload() {
 
 }
-function setup(){
-    canvas = createCanvas(300, 300);
+
+function setup() {
+    canvas = createCanvas(400, 300);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.size(400, 300);
+    video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", gotPoses);
 }
-function draw(){
 
+function draw() {
+    image(video, 0, 0, 400, 300);
 }
-function take_snapshot(){
+
+function modelLoaded() {
+    console.log("PoseNet is initialized");
+}
+
+function gotPoses(){
+    if (results.length > 0){
+        console.log(results);
+        console.log("Nose x = " + results[0].pose.nose.x);
+        console.log("Nose y = " + results[0].pose.nose.y);
+    }
+}
+
+function take_snapshot() {
     save("face-filter-image.png");
 }
