f73d77df4ca583ef1110bf5a6b487113b5e438ee
Rocket-Octane <93807708+Rocket-Octane@users.noreply.github.com>
2022-06-18 20:28:09 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-function preload() {
+noseX = 0;
+noseY = 0;
 
+function preload() {
+    moustache = loadImage("https://i.postimg.cc/3x3QzSGq/m.png");
 }
 
 function setup() {
@@ -12,20 +15,23 @@ function setup() {
     poseNet.on("pose", gotPoses);
 }
 
-function draw() {
-    image(video, 0, 0, 400, 300);
-}
-
 function modelLoaded() {
     console.log("PoseNet is initialized");
 }
 
-function gotPoses(){
+function gotPoses(results){
     if (results.length > 0){
         console.log(results);
-        console.log("Nose x = " + results[0].pose.nose.x);
-        console.log("Nose y = " + results[0].pose.nose.y);
+        noseX = results[0].pose.nose.x - 40;
+        noseY = results[0].pose.nose.y - 5;
+        console.log("Nose x = " + noseX);
+        console.log("Nose y = " + noseY);
     }
+}
+
+function draw() {
+    image(video, 0, 0, 400, 300);
+    image(moustache, noseX, noseY, 80, 40);
 }
 
 function take_snapshot() {
